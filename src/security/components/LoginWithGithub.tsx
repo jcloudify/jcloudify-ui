d124ee8b55522e7eafb314e787872110ed4af088
Yume <hei.hajatiana@gmail.com>
2024-05-21 10:41:26 +0300

    fix: LoginWithGithub should remain responsive

--- src/security/components/LoginWithGithub.tsx
+++ src/security/components/LoginWithGithub.tsx
@@ -1,7 +1,8 @@
 import {Button} from "react-admin";
-import {Stack, Box, Typography} from "@mui/material";
+import {Stack, Box, Typography, useMediaQuery, Theme} from "@mui/material";
 import {GitHub} from "@mui/icons-material";
 import {colors} from "@/themes";
+import {gh} from "@/config/env";
 
 const Octagon: React.FC<{
   top?: number;
@@ -26,23 +27,29 @@ const Octagon: React.FC<{
   />
 );
 
+const getLoginWithGithubUrl = () =>
+  `https://github.com/login/oauth/authorize?client_id=${gh.clientId}`;
+
 export const LoginWithGithub: React.FC = () => {
+  const mdScreen = useMediaQuery("(min-width:990px)");
   return (
     <Stack height="100vh" width="100%" bgcolor="red" direction="row">
-      <Stack
-        height="100%"
-        width="100%"
-        justifyContent="center"
-        alignItems="center"
-        sx={{
-          background:
-            "linear-gradient(110.6deg, rgb(156, 116, 129) -18.3%, rgb(67, 54, 74) 16.4%, rgb(47, 48, 67) 68.2%, rgb(27, 23, 36) 99.1%)",
-        }}
-      >
-        <Typography variant="h3" fontWeight="600" color="#fff">
-          Build for the future
-        </Typography>
-      </Stack>
+      {mdScreen && (
+        <Stack
+          height="100%"
+          width="100%"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            background:
+              "linear-gradient(110.6deg, rgb(156, 116, 129) -18.3%, rgb(67, 54, 74) 16.4%, rgb(47, 48, 67) 68.2%, rgb(27, 23, 36) 99.1%)",
+          }}
+        >
+          <Typography variant="h3" fontWeight="600" color="#fff">
+            Build for the future
+          </Typography>
+        </Stack>
+      )}
 
       <Stack
         height="100%"
@@ -67,7 +74,7 @@ export const LoginWithGithub: React.FC = () => {
             variant="contained"
             startIcon={<GitHub />}
             label="Login with Github"
-            href="/"
+            href={getLoginWithGithubUrl()}
           />
           <Octagon top={100} left={-150} color={colors("gray-1")} />
           <Octagon bottom={-30} right={-30} />
