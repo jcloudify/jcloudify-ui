1a1aedc21829ec5dad2d1ced96d073fd0576cbca
Rojotiana Nomena <hei.nomena@gmail.com>
2024-05-23 14:17:02 +0300

    feat: planView

---
+++ src/operations/plan/Plan.tsx
@@ -0,0 +1,26 @@
+import {Box, Stack} from "@mui/material";
+import {PlanCard} from "./PlanCard";
+import {Plan} from "@jcloudify-api/typescript-client";
+import {useState} from "react";
+
+interface PlanViewProps {
+  plans: Plan[];
+}
+
+export const PlanView: React.FC<PlanViewProps> = ({plans}) => {
+  const [activePlan, setActivePlan] = useState(plans[0].name!);
+
+  return (
+    <Box sx={{mx: "auto"}}>
+      <Stack direction="row" spacing={1}>
+        {plans.map((plan) => (
+          <PlanCard
+            plan={plan}
+            activePlan={activePlan}
+            setActivePlan={setActivePlan}
+          />
+        ))}
+      </Stack>
+    </Box>
+  );
+};
