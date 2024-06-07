944a845f317d6c9767abc6b9344a47355c594f88
Yume <hei.hajatiana@gmail.com>
2024-06-07 19:37:41 +0300

    refactor: route renderer

---
+++ src/components/router.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {Route} from "react-router-dom";
+
+export type RouteMap = {
+  [k: string]: JSX.Element | RouteMap;
+};
+
+/* utility */
+export const renderRouteMap = (rm: RouteMap) => {
+  const keys = Object.keys(rm);
+
+  return keys.map((key) => {
+    const v = rm[key];
+
+    if (isElement(v)) {
+      const index = key === "$$index";
+      return (
+        <Route
+          key={key}
+          index={index}
+          path={index ? undefined : key}
+          element={v}
+        />
+      );
+    }
+
+    return <Route path={key}>{renderRouteMap(v as RouteMap)}</Route>;
+  });
+};
+
+const isElement = (val: any): val is React.ReactElement => "$$typeof" in val;
