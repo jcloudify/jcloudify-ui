1d142bbab8b54af4777ad9851db3e991a1cd022a
Rojotiana Nomena <hei.nomena@gmail.com>
2024-06-14 16:38:55 +0300

    fix: log list display

--- src/operations/apps/AppLogs.tsx
+++ src/operations/apps/AppLogs.tsx
@@ -1,56 +1,9 @@
-import {useState} from "react";
-import {useGetList} from "react-admin";
 import {useParams, useSearchParams} from "react-router-dom";
-import {
-  Box,
-  Card,
-  CardHeader,
-  CardContent,
-  Divider,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
-} from "@mui/material";
 import {LogList, LogShow} from "@/operations/logs";
 import {WithTab} from "@/components/tab";
-import {colors} from "@/themes";
 
 export const AppLogList: React.FC = () => {
-  const {appId} = useParams();
-
-  const {data: envList = []} = useGetList("environments", {
-    meta: {application_id: appId},
-  });
-
-  const [envId, setEnvId] = useState(envList.length ? envList[0].id : "");
-
-  return (
-    <Card component={Box} flex={1} height="100%">
-      <CardHeader title="Logs" />
-      <Divider sx={{borderColor: colors("gray-0")}} />
-      <CardContent>
-        <Stack direction="row" justifyContent="flex-end">
-          <FormControl>
-            <InputLabel id="env-select-label">Environment</InputLabel>
-            <Select
-              labelId="env-select-label"
-              value={envId}
-              label="Environment"
-              onChange={(e) => setEnvId(e.target.value)}
-              variant="outlined"
-            >
-              {envList.map(({id, environment_type}) => (
-                <MenuItem value={id}>{environment_type}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Stack>
-        <LogList exporter={false} envId={envId} pagination={false} title=" " />
-      </CardContent>
-    </Card>
-  );
+  return <LogList exporter={false} pagination={false} title=" " />;
 };
 
 export const AppLogShow: React.FC = () => {
--- src/operations/logs/LogList.tsx
+++ src/operations/logs/LogList.tsx
@@ -1,57 +1,29 @@
 import {
   Datagrid,
   DateField,
-  DateInput,
   Identifier,
   List,
   ListProps,
   RaRecord,
-  SelectInput,
   TextField,
   UrlField,
 } from "react-admin";
-import {LogLogTypeEnum} from "@jcloudify-api/typescript-client";
 
 export type LogListProps<Record extends RaRecord<Identifier> = any> = Omit<
   ListProps<Record>,
   "resource" | "children"
-> & {
-  envId: string;
-};
+>;
 
-const logFilters = [
-  <SelectInput
-    alwaysOn
-    label="Type"
-    source="log_type"
-    optionValue="name"
-    choices={[
-      {name: LogLogTypeEnum.APPLICATION_LOG},
-      {name: LogLogTypeEnum.DEPLOYMENT_LOG},
-    ]}
-  />,
-  <DateInput source="start_date_time" />,
-  <DateInput source="end_date_time" />,
-];
-
-export const LogList: React.FC<LogListProps> = ({
-  envId: environment_id,
-  queryOptions = {},
-  ...rest
-}) => {
-  queryOptions.meta ||= {};
-
+export const LogList: React.FC<LogListProps> = ({...rest}) => {
+  const environment_id = "prod_env";
   return (
     <List
       resource="logs"
       queryOptions={{
-        ...queryOptions,
         meta: {
-          ...queryOptions.meta,
           environment_id,
         },
       }}
-      filters={logFilters}
       {...rest}
     >
       <Datagrid rowClick={(id) => `${id}?envId=${environment_id}`}>
