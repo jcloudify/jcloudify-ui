4a53cb7742bb644b900b2333a55794720c0280d5
Rojotiana Nomena <hei.nomena@gmail.com>
2024-06-18 16:53:17 +0300

    fix: select environment id

--- src/operations/logs/LogList.tsx
+++ src/operations/logs/LogList.tsx
@@ -1,29 +1,58 @@
 import {
   Datagrid,
   DateField,
+  Filter,
   Identifier,
   List,
   ListProps,
   RaRecord,
+  SelectInput,
   TextField,
   UrlField,
+  required,
+  useGetList,
 } from "react-admin";
+import {Stack} from "@mui/material";
+import {Environment} from "@jcloudify-api/typescript-client";
+import {useParams} from "react-router-dom";
 
 export type LogListProps<Record extends RaRecord<Identifier> = any> = Omit<
   ListProps<Record>,
   "resource" | "children"
 >;
 
+const LogListFilter: React.FC<{envs: Environment[]}> = ({envs}) => {
+  return (
+    <Stack direction="column" spacing={3} alignItems="flex-end">
+      <Filter>
+        <SelectInput
+          alwaysOn
+          source="environment_id"
+          validate={required()}
+          choices={envs}
+          label="Environment"
+          optionValue="id"
+          optionText="environment_type"
+        />
+      </Filter>
+    </Stack>
+  );
+};
+
 export const LogList: React.FC<LogListProps> = ({...rest}) => {
   const environment_id = "prod_env";
+  const {appId} = useParams();
+  const {data: envs = []} = useGetList("environments", {
+    meta: {application_id: appId},
+  });
+
+  if (!envs.length) return;
+
   return (
     <List
       resource="logs"
-      queryOptions={{
-        meta: {
-          environment_id,
-        },
-      }}
+      actions={<LogListFilter envs={envs} />}
+      filterDefaultValues={{environment_id: envs[0].id}}
       {...rest}
     >
       <Datagrid rowClick={(id) => `${id}?envId=${environment_id}`}>
--- src/providers/logsProvider.ts
+++ src/providers/logsProvider.ts
@@ -3,8 +3,8 @@ import {PojaDataProvider} from "./types";
 import {logs} from "#/logs.mock";
 
 export const logsProvider: PojaDataProvider<Required<Log>> = {
-  getList(_page, _perPage, _filter, meta) {
-    return Promise.resolve(logs(meta!.environment_id));
+  getList(_page, _perPage, filter, _meta) {
+    return Promise.resolve(logs(filter?.environment_id));
   },
   getOne(id, meta): Promise<any> {
     return Promise.resolve(
