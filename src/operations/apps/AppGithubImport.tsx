fe5a08bd5e8736418f77e0622dd833da49419296
Rojotiana Nomena <hei.nomena@gmail.com>
2024-06-20 14:07:57 +0300

    feat: repository view

--- src/operations/apps/AppGithubImport.tsx
+++ src/operations/apps/AppGithubImport.tsx
@@ -1,15 +1,22 @@
 import {
+  Avatar,
+  Button,
+  Divider,
   InputAdornment,
   ListItemIcon,
   MenuItem,
+  Paper,
   Select,
   Stack,
   TextField,
+  Typography,
 } from "@mui/material";
 import {GitHub as GitHubIcon, Search as SearchIcon} from "@mui/icons-material";
 import {Heading} from "@/components/head";
 import {ContainerWithHeading} from "@/components/container";
-import {ghOrg} from "#/github.mock";
+import {fromToNow} from "@/utils/date";
+
+import {ghOrg, ghRepository} from "#/github.mock";
 
 export const AppGithubImport: React.FC = () => {
   return (
@@ -30,31 +37,60 @@ export const AppGithubImport: React.FC = () => {
 const ImportGhRepository = () => {
   return (
     <ContainerWithHeading title="Import Git Repository">
-      <Stack direction="row" spacing={2}>
-        <Select fullWidth defaultValue={ghOrg[0].id}>
-          {ghOrg.map((org) => (
-            <MenuItem value={org.id} key={`org-${org.id}`}>
-              <ListItemIcon>
-                <GitHubIcon />
-              </ListItemIcon>
-              {org.username}
-            </MenuItem>
+      <Stack direction="column" spacing={2}>
+        <Stack direction="row" spacing={2}>
+          <Select fullWidth defaultValue={ghOrg[0].id}>
+            {ghOrg.map((org) => (
+              <MenuItem value={org.id} key={`org-${org.id}`}>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                {org.username}
+              </MenuItem>
+            ))}
+          </Select>
+          <TextField
+            label="Search"
+            size="medium"
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
+        <Stack direction="column" spacing={1}>
+          {ghRepository.map((repo) => (
+            <RepositoryView repo={repo} key={`repo-${repo.id}`} />
           ))}
-        </Select>
-        <TextField
-          label="Search"
-          size="medium"
-          fullWidth
-          variant="outlined"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
+        </Stack>
       </Stack>
     </ContainerWithHeading>
+  );
+};
+
+const RepositoryView: React.FC<{repo: any}> = ({repo}) => {
+  return (
+    <Stack
+      direction="row"
+      spacing={1}
+      alignItems="center"
+      p={2}
+      component={Paper}
+      justifyContent="space-between"
+    >
+      <Stack direction="row" spacing={1} alignItems="center">
+        <Avatar>
+          <GitHubIcon />
+        </Avatar>
+        <Typography variant="h6">{repo.name}</Typography>
+        <Typography variant="body2">{fromToNow(repo.created_at)}</Typography>
+      </Stack>
+      <Button variant="contained">Import</Button>
+    </Stack>
   );
 };
