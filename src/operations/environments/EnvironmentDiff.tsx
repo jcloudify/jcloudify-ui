09fbe325fda1d3094675ff23a8581ab7a605570a
Yume <hei.hajatiana@gmail.com>
2024-07-19 11:48:00 +0300

    refactor: DiffViewer, EnvironmentDiff

--- src/operations/environments/EnvironmentDiff.tsx
+++ src/operations/environments/EnvironmentDiff.tsx
@@ -1,106 +1,73 @@
 import {Environment} from "@jcloudify-api/typescript-client";
 import {useState} from "react";
 import {useGetList} from "react-admin";
-import {Select, MenuItem, SelectChangeEvent} from "@mui/material";
-import Prism from "prismjs";
-import "@/themes/prism/github-light.css";
-import DiffViewer, {DiffMethod} from "react-diff-viewer-continued";
+import {Box, Paper, Stack, Select, MenuItem} from "@mui/material";
+
+import {DiffViewer} from "@/components/diff";
+import {Heading} from "@/components/head";
+
 import {ToRecord} from "@/providers";
+import {highlightJSON} from "@/config/prism";
+import {prettyJSON} from "@/utils/format";
 
 export const EnvironmentDiff: React.FC<{appId: string}> = ({appId}) => {
-  const [environmentPair, setEnvironmentPair] = useState<
-    [Environment, Environment]
-  >([] as any);
+  const [toCompare, setToCompare] = useState<Environment[]>([]);
 
   const {data: environments = []} = useGetList<ToRecord<Environment>>(
     "environments",
     {meta: {application_id: appId}}
   );
 
-  const setEnvironment = (ev: SelectChangeEvent<string>, idx: number) => {
-    const id = ev.target.value;
-    const newEnvironmentPair = [...environmentPair] as [
-      Environment,
-      Environment,
-    ];
-    newEnvironmentPair[idx] = environments.find(
-      (environment) => environment.id === id
+  const setDiffEnvironment = (selectedId: string, toCompareIdx: number) => {
+    toCompare[toCompareIdx] = environments.find(
+      (environment) => environment.id === selectedId
     )!;
-    setEnvironmentPair(newEnvironmentPair);
+    setToCompare(toCompare.slice());
   };
 
-  const [conf1, conf2] = environmentPair;
+  const [c1, c2] = toCompare;
 
   return (
-    <DiffViewer
-      compareMethod={DiffMethod.WORDS}
-      leftTitle={
-        <Select<string> onChange={(ev) => setEnvironment(ev, 0)} size="small">
-          {environments.map((environment) => (
-            <MenuItem key={environment.id} value={environment.id}>
-              {environment.environment_type}
-            </MenuItem>
-          ))}
-        </Select>
-      }
-      rightTitle={
-        <Select<string> onChange={(ev) => setEnvironment(ev, 1)} size="small">
-          {environments.map((environment) => (
-            <MenuItem key={environment.id} value={environment.id}>
-              {environment.environment_type}
-            </MenuItem>
-          ))}
-        </Select>
-      }
-      oldValue={conf1 ? toPrettyJSON(conf1) : ""}
-      newValue={conf2 ? toPrettyJSON(conf2) : ""}
-      renderContent={highlightJSON}
-    />
+    <Stack mt={4} mb={3} spacing={3}>
+      <Heading
+        title="Environment Diff"
+        subtitle="Compare Environment Differences"
+        p={2}
+        size="sm"
+      />
+
+      <Box component={Paper} bgcolor="#fff">
+        <DiffViewer
+          leftTitle={
+            <SelectEnvironmentToCompare
+              environments={environments}
+              onSelect={(id) => setDiffEnvironment(id, 0)}
+            />
+          }
+          rightTitle={
+            <SelectEnvironmentToCompare
+              environments={environments}
+              onSelect={(id) => setDiffEnvironment(id, 1)}
+            />
+          }
+          oldValue={c1 ? prettyJSON(c1) : ""}
+          newValue={c2 ? prettyJSON(c2) : ""}
+          highlight={highlightJSON}
+        />
+      </Box>
+    </Stack>
   );
 };
 
-Prism.languages.json = {
-  property: {
-    pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?=\s*:)/,
-    lookbehind: true,
-    greedy: true,
-    inside: {
-      operator: /"/,
-    },
-  },
-  string: {
-    pattern: /(^|[^\\])"(?:\\.|[^\\"\r\n])*"(?!\s*:)/,
-    lookbehind: true,
-    greedy: true,
-    inside: {
-      operator: /"/,
-    },
-  },
-
-  comment: {
-    pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,
-    greedy: true,
-  },
-  number: /-?\b\d+(?:\.\d+)?(?:e[+-]?\d+)?\b/i,
-  punctuation: /[{}[\],]/,
-  operator: /:/,
-
-  boolean: /\b(?:false|true)\b/,
-  null: {
-    pattern: /\bnull\b/,
-    alias: "keyword",
-  },
-};
-
-const highlightJSON = (s: string) => (
-  <pre
-    style={{display: "inline"}}
-    dangerouslySetInnerHTML={{
-      __html: Prism.highlight(s || "", Prism.languages.json, "json"),
-    }}
-  />
+const SelectEnvironmentToCompare: React.FC<{
+  environments: Environment[];
+  onSelect: (id: string) => void;
+}> = ({environments, onSelect}) => (
+  <Select<string> onChange={(ev) => onSelect(ev.target.value)} size="small">
+    {environments.map((environment) => (
+      <MenuItem key={environment.id} value={environment.id}>
+        {environment.environment_type}
+      </MenuItem>
+    ))}
+  </Select>
 );
-
-const toPrettyJSON = (o: any) => {
-  return JSON.stringify(o, null, 2);
-};
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,3 +1,7 @@
 export const ms = (val: any) => {
   return `${val.toLocaleString()}ms`;
 };
+
+export const prettyJSON = (rawObj: object) => {
+  return JSON.stringify(rawObj, null, 2);
+};
