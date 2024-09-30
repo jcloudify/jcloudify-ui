96fa2da572d9c2402d30291a12e193e213a8b3cd
Yume <hei.hajatiana@gmail.com>
2024-09-30 11:21:20 +0300

    fix: show unable to create warning on env creation when entering through url directly

--- src/operations/environments/EnvironmentCreation.tsx
+++ src/operations/environments/EnvironmentCreation.tsx
@@ -36,64 +36,74 @@ export const EnvironmentCreation: React.FC<{appId: string}> = ({appId}) => {
         size="sm"
       />
 
-      {!canCreateMore && (
+      {canCreateMore ? (
+        <Box>
+          <GridLayout xs={6} spacing={1.5}>
+            <Stack component={Paper} p={2} height="100%">
+              <Heading title="New" subtitle="Create from scratch" size="sm" />
+
+              <Box>
+                <Button
+                  onClick={navigateToCreateEnv}
+                  label="Create"
+                  variant="outlined"
+                  size="medium"
+                  disabled={!canCreateMore}
+                  data-testid="CreateFromScratch"
+                />
+              </Box>
+            </Stack>
+
+            <Stack
+              component={Paper}
+              p={2}
+              spacing={2}
+              height="100%"
+              sx={{opacity: !canCreateMore ? 0.9 : 1}}
+            >
+              <Heading
+                title="From an Existing"
+                subtitle="Create an environment from an existing one"
+                size="sm"
+              />
+
+              {canCreateMore && !!createdEnvironments.length && (
+                <>
+                  <Select
+                    size="small"
+                    variant="outlined"
+                    data-testid="select-creation-template"
+                    onChange={(ev) => setTemplateId(ev.target.value as string)}
+                    disabled={!createdEnvironments.length || !canCreateMore}
+                  >
+                    {createdEnvironments.map((env) => (
+                      <MenuItem key={env.id} value={env.id}>
+                        <EnvironmentType value={env.environment_type!} />
+                      </MenuItem>
+                    ))}
+                  </Select>
+
+                  <Box>
+                    <Button
+                      disabled={!templateId || !canCreateMore}
+                      onClick={navigateToCreateEnv}
+                      label="Create"
+                      variant="outlined"
+                      size="medium"
+                      data-testid="CreateFromExisting"
+                    />
+                  </Box>
+                </>
+              )}
+            </Stack>
+          </GridLayout>
+        </Box>
+      ) : (
         <Alert severity="error">
           All available environments have been already created, so it's not
           possible to create more at this time.
         </Alert>
       )}
-
-      <Box>
-        <GridLayout xs={6} spacing={1.5}>
-          <Stack component={Paper} p={2} height="100%">
-            <Heading title="New" subtitle="Create from scratch" size="sm" />
-
-            <Box>
-              <Button
-                onClick={navigateToCreateEnv}
-                label="Create"
-                variant="outlined"
-                size="medium"
-                disabled={!canCreateMore}
-                data-testid="CreateFromScratch"
-              />
-            </Box>
-          </Stack>
-
-          <Stack component={Paper} p={2} spacing={2} height="100%">
-            <Heading
-              title="From an Existing"
-              subtitle="Create an environment from an existing one"
-              size="sm"
-            />
-
-            <Select
-              size="small"
-              variant="outlined"
-              data-testid="select-creation-template"
-              onChange={(ev) => setTemplateId(ev.target.value as string)}
-              disabled={!createdEnvironments.length || !canCreateMore}
-            >
-              {createdEnvironments.map((env) => (
-                <MenuItem key={env.id} value={env.id}>
-                  <EnvironmentType value={env.environment_type!} />
-                </MenuItem>
-              ))}
-            </Select>
-
-            <Box>
-              <Button
-                disabled={!templateId || !canCreateMore}
-                onClick={navigateToCreateEnv}
-                label="Create"
-                variant="outlined"
-                size="medium"
-                data-testid="CreateFromExisting"
-              />
-            </Box>
-          </Stack>
-        </GridLayout>
-      </Box>
     </Stack>
   );
 };
