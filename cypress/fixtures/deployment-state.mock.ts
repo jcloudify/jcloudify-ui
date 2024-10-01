1581bc88601ea8e4c0392bd8f69dc37fc77d7c1d
Yume <hei.hajatiana@gmail.com>
2024-10-01 12:41:11 +0300

    style: reformat

--- cypress/fixtures/deployment-state.mock.ts
+++ cypress/fixtures/deployment-state.mock.ts
@@ -1,4 +1,7 @@
-import {DeploymentState, DeploymentStateEnum} from "@jcloudify-api/typescript-client";
+import {
+  DeploymentState,
+  DeploymentStateEnum,
+} from "@jcloudify-api/typescript-client";
 import {nanoid} from "nanoid";
 
 const chainedStateFromStateArray = (states: DeploymentStateEnum[]) => {
@@ -14,14 +17,16 @@ const chainedStateFromStateArray = (states: DeploymentStateEnum[]) => {
       progressionStatus: states.shift(),
       executionType: "ASYNCHRONOUS",
       nextState: undefined,
-    }
+    };
     current.nextState = nextState;
     current = nextState;
   }
   return deploymentState;
-}
+};
 
-export const templateCheck_inProgress = chainedStateFromStateArray([DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS]);
+export const templateCheck_inProgress = chainedStateFromStateArray([
+  DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS,
+]);
 export const provision_inProgress = chainedStateFromStateArray([
   DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS,
   DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_INITIATED,
@@ -46,18 +51,18 @@ export const templateCheck_failed = chainedStateFromStateArray([
   DeploymentStateEnum.TEMPLATE_FILE_CHECK_FAILED,
 ]);
 export const provision_failed = chainedStateFromStateArray([
-    DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_INITIATED,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_IN_PROGRESS,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYED,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_FAILED,
+  DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_INITIATED,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_IN_PROGRESS,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYED,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_FAILED,
 ]);
 
 export const deploy_failed = chainedStateFromStateArray([
-    DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_INITIATED,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_IN_PROGRESS,
-    DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYED,
-    DeploymentStateEnum.COMPUTE_STACK_DEPLOYMENT_IN_PROGRESS,
-    DeploymentStateEnum.COMPUTE_STACK_DEPLOYMENT_FAILED
+  DeploymentStateEnum.TEMPLATE_FILE_CHECK_IN_PROGRESS,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_INITIATED,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYMENT_IN_PROGRESS,
+  DeploymentStateEnum.INDEPENDENT_STACKS_DEPLOYED,
+  DeploymentStateEnum.COMPUTE_STACK_DEPLOYMENT_IN_PROGRESS,
+  DeploymentStateEnum.COMPUTE_STACK_DEPLOYMENT_FAILED,
 ]);
