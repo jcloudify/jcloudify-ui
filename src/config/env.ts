ac8f79f2afd43d4d8df7422a08c8ba1db3d487a5
Yume <hei.hajatiana@gmail.com>
2024-10-03 11:52:03 +0300

    fix!: update github app installationLink

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,8 @@
 export const gh = {
   clientId: process.env.GH_APP_CLIENT_ID,
   clientSecret: process.env.GH_APP_CLIENT_SECRET,
-  installationLink: "https://github.com/apps/jcloudify/installations/new",
+  installationLink:
+    "https://github.com/apps/preprod-jcloudify/installations/new",
 };
 
 export const baseUrl = process.env.GH_APP_CLIENT_ID!;
