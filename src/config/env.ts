363cc9f0a8bf35592ea0f55c202d11bdae8f66de
Yume <hei.hajatiana@gmail.com>
2024-10-08 13:43:04 +0300

    chore: gihtub_app_installation_link -> env.GH_APP_INSTALLATION_LINK

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 export const gh = {
-  clientId: process.env.GH_APP_CLIENT_ID,
-  clientSecret: process.env.GH_APP_CLIENT_SECRET,
-  installationLink: "https://github.com/apps/jcloudify/installations/new",
+  clientId: process.env.GH_APP_CLIENT_ID!,
+  clientSecret: process.env.GH_APP_CLIENT_SECRET!,
+  installationLink: process.env.GH_APP_INSTALLATION_LINK!,
 };
 
 export const baseUrl = process.env.GH_APP_CLIENT_ID!;
