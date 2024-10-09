18ab1401a0d700e66860d23c15848770effeb30d
Rojotiana Nomena <hei.nomena@gmail.com>
2024-10-09 15:44:57 +0300

    feat: billing resource

---
+++ src/operations/billing/index.ts
@@ -0,0 +1,12 @@
+import {Payment as PaymentIcon} from "@mui/icons-material";
+import {Operation} from "@/operations/types";
+import {Payments} from "./Payments";
+
+export const billing: Operation = {
+  name: "billing",
+  list: Payments,
+  icon: PaymentIcon,
+  options: {label: "Billing"},
+};
+
+export * from "./Payments";
