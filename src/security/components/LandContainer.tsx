74c67e2caebbb32902a6ff8bd0b2d60a7f7e9eba
Yume <hei.hajatiana@gmail.com>
2024-10-17 14:21:17 +0300

    feat: add docs button

--- src/security/components/LandContainer.tsx
+++ src/security/components/LandContainer.tsx
@@ -1,11 +1,16 @@
-import {Stack, Typography, useMediaQuery} from "@mui/material";
+import {IconButtonWithTooltip} from "react-admin";
+import {MdHome} from "react-icons/md";
+import {Box, Stack, Typography, useMediaQuery} from "@mui/material";
 import {colors} from "@/themes";
 import {Octagon} from "@/components/shape";
+import {redirect} from "@/utils/redirect";
+import {jcloudifyWebsiteUrl} from "@/config/env";
 
 export const LandContainer: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   const mdScreen = useMediaQuery("(min-width:990px)");
+
   return (
     <Stack height="100vh" width="100%" bgcolor="red" direction="row">
       {mdScreen && (
@@ -30,9 +35,20 @@ export const LandContainer: React.FC<React.PropsWithChildren> = ({
         overflow="hidden"
         direction="column"
       >
-        <Typography variant="h3" fontWeight="600" p={3}>
-          JCloudify
-        </Typography>
+        <Stack width="100%" direction="row" alignItems="center" p={3}>
+          <Typography variant="h3" fontWeight="600" flex={1}>
+            JCloudify
+          </Typography>
+
+          <Box>
+            <IconButtonWithTooltip
+              label="Home"
+              onClick={() => redirect(jcloudifyWebsiteUrl)}
+            >
+              <MdHome size={20} />
+            </IconButtonWithTooltip>
+          </Box>
+        </Stack>
         {children}
         <Octagon top={100} left={-150} color={colors("gray-1")} />
         <Octagon bottom={-30} right={-30} />
