387eea7dcae38ea59e886be2d873112f6b08ef43
Yume <hei.hajatiana@gmail.com>
2024-10-20 15:50:16 +0300

    style: remove console.log

--- src/operations/environments/useCreateEnvironmentWithConfig.ts
+++ src/operations/environments/useCreateEnvironmentWithConfig.ts
@@ -55,18 +55,6 @@ export const useCreateEnvironmentWithConfig = (
       return;
     }
 
-    console.log("before_transform_conf", params.config);
-    console.log(
-      "after_transform_conf",
-      pcc.formTransformFormValues(
-        {
-          to_create: params.environment,
-          ...params.config,
-        },
-        targetApp!
-      )
-    );
-
     await create(
       "environments",
       {
