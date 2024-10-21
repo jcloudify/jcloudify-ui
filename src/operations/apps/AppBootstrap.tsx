f85b1ade48e1d29404c62bd31521ca6320251c21
Yumii <hei.hajatiana@gmail.com>
2024-10-21 17:14:54 +0300

    fix: put package_full_name under preprod conf in appbootstrap
    * test: check pkg_name from app.bootstrap env_conf saving payload
    
    * fix(app_bootstrap): inject pkg_name into env conf save payload
    
    * test(fix): clearn pkg_name field before type
    
    * style: reformat

--- src/operations/apps/AppBootstrap.tsx
+++ src/operations/apps/AppBootstrap.tsx
@@ -2,7 +2,7 @@ import {
   EnvironmentType as EnvironmentTypeEnum,
   GithubAppInstallation,
 } from "@jcloudify-api/typescript-client";
-import {useState, useMemo, useEffect} from "react";
+import {useState, useMemo, useEffect, useRef} from "react";
 import {
   CreateBase,
   Form,
@@ -55,6 +55,8 @@ export const AppBootstrap: React.FC = () => {
 
   const [hasCreatedApp, setHasCreatedApp] = useState(false);
 
+  const packageNameRef = useRef("");
+
   const newAppId = useMemo(() => nanoid(), []);
   const newEnvironmentId = useMemo(() => nanoid(), []);
 
@@ -78,7 +80,13 @@ export const AppBootstrap: React.FC = () => {
 
     createEnvironmentWithConfig(
       {
-        config: pcc.formDefaultValues!,
+        config: {
+          ...pcc.formDefaultValues!,
+          general: {
+            ...pcc.formDefaultValues!.general,
+            package_full_name: packageNameRef.current,
+          },
+        },
         environment: {
           id: newEnvironmentId,
           environment_type: EnvironmentTypeEnum.PREPROD,
@@ -122,8 +130,9 @@ export const AppBootstrap: React.FC = () => {
       <CreateBase
         resource="applications"
         mutationOptions={{
-          onSuccess: () => {
+          onSuccess: (app) => {
             setHasCreatedApp(true);
+            packageNameRef.current = app.package_name;
           },
         }}
       >
