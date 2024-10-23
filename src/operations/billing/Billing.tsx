8aa1e7768f02af0a1935d83faec7bba5863d55dd
Rojotiana Nomena <hei.nomena@gmail.com>
2024-10-23 23:48:44 +0300

    fix: billing info

--- src/operations/billing/Billing.tsx
+++ src/operations/billing/Billing.tsx
@@ -1,27 +1,10 @@
-import {Box, Stack} from "@mui/material";
-import {ContainerWithHeading} from "@/components/container";
-import {PaymentDetailsShow} from "@/operations/payments";
+import {Title} from "react-admin";
+import {Stack} from "@mui/material";
 import {BillingInfoShow} from "@/operations/billing";
 
 export const Billing: React.FC = () => (
   <Stack direction="column" spacing={1}>
-    <BillingSummary />
+    <Title title="Billing" />
+    <BillingInfoShow />
   </Stack>
-);
-
-const BillingSummary: React.FC = () => (
-  <ContainerWithHeading title="Payment Details Summary">
-    <Stack
-      direction={{sm: "column", md: "row"}}
-      spacing={1}
-      alignItems="stretch"
-    >
-      <Box sx={{width: "100%"}}>
-        <BillingInfoShow />
-      </Box>
-      <Box sx={{width: "100%"}}>
-        <PaymentDetailsShow />
-      </Box>
-    </Stack>
-  </ContainerWithHeading>
 );
